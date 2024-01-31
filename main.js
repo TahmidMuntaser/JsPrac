c10713f1b46ee54b5792099240fec514404b12be
Tahmid Muntaser <115565668+TahmidMuntaser@users.noreply.github.com>
2024-02-01 04:41:08 +0600

    js p1 up2.0

---
+++ main.js
@@ -0,0 +1,13 @@
+alert("Test Alert!!");
+
+console.log('Hello world!!');
+console.error('Error!');
+console.warn('Warning!');
+// view in console
+// website: mdn console
+
+
+
+
+document.write('Hello!!');
+// view in dom area
