bfb225397a3a3a56c02ac0a46b990327b88d04cf
Tahmid Muntaser <115565668+TahmidMuntaser@users.noreply.github.com>
2024-02-01 05:08:09 +0600

    js p2 up1.0

--- main.js
+++ main.js
@@ -1,10 +1,55 @@
-alert("Test Alert!!");
+// alert("Test Alert!!");
 
 console.log('Hello world!!');
 console.error('Error!');
 console.warn('Warning!');
 // view in console
 // website: mdn console
+
+let age = 4;
+age = 33;
+let num;
+
+// const a; error
+const a = 25;
+// a = 33; can't change the value
+console.log(a);
+
+console.log(age);
+console.log(num); // undefined
+
+
+var q = 33;
+q = 1;
+
+console.log(q);
+
+
+function start(){
+    for(let i = 0; i<5; ++i){
+        console.log(i);
+    }
+    // let = block scopped
+    // console.log(i); error
+}
+
+function start2(){
+    for(var i = 0; i<5; ++i){
+        console.log(i);
+    }
+    // var = function scopped
+    // available in window obj
+    console.log(i); //no error 
+}
+
+start();
+start2();
+
+
+var color = 'red';
+// now it is available in window obj
+let age1 = 33;
+
 
 
 
