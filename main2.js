428ea56e56890752c0dfa4cccb108fbbea099594
Tahmid Muntaser <115565668+TahmidMuntaser@users.noreply.github.com>
2024-02-03 01:13:16 +0600

    js p6 upt1.0

--- main2.js
+++ main2.js
@@ -10,8 +10,48 @@ console.log(x,y,z);
 //arithmetic ope
 
 // ** exponential ope
-
-
 console.log(x**y);
+
+/*
+    Car -> obj
+    properties:
+        brand: ,
+        model: ,
+        colors: ,
+        weight: 
+
+
+    method:
+        start()
+        drive()
+        break()
+
+*/
+
+const person = { //obj
+
+    //properties:
+    fristName: 'John', //camelCase
+    lastname: 'Vanston',
+    age: 25,
+    hobbies: ['music', 'gaming', 'movies', 'travel'],
+    
+    //another obj
+    address: {
+        city:'Dhaka',
+        zip: 1000
+    }
+
+
+}
+
+
+console.log(person);
+console.log(person.fristName, person.lastname);
+
+console.log(person.hobbies[1]);
+console.log(person.address.city);
+console.log(person.address);
+
 
 
