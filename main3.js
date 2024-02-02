f5239df31e6876f3ed52777f01b5c8050f445d3a
Tahmid Muntaser <115565668+TahmidMuntaser@users.noreply.github.com>
2024-02-03 02:51:34 +0600

    p8 upt.00

--- main3.js
+++ main3.js
@@ -16,3 +16,34 @@ if(x === 10) { //same value and datatype
 if(y !== 1) { //same value and datatype
     console.log(y);
 }
+
+const q = 12;
+if(q == 12){
+    console.log('q = 12');
+}
+else if(q>10){
+    console.log('q > 10');
+}
+else{
+    console.log('q is not 12');
+}
+
+const p = 2;
+
+const res = p>=2 ? 'NO' : 'YES';
+console.log(res);
+
+
+switch (res) {
+    case('YES'):
+        console.log('OMG its YES!!!');
+        break;
+
+    case('NO'):
+    console.log('Its NO, Come on...');
+    break;
+
+    default:
+        console.log('I dont give a shit');
+        break;
+}
