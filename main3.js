2695dea7d4a65435a1c3e6ab776c857378cf2b32
Tahmid Muntaser <115565668+TahmidMuntaser@users.noreply.github.com>
2024-02-03 02:22:09 +0600

    js p7 upt1.00

---
+++ main3.js
@@ -0,0 +1,18 @@
+const x = '10';
+const y = '1';
+
+
+if(x == 10) {
+    console.log(x);
+}
+if(y != 10) {
+    console.log(5);
+}
+
+if(x === 10) { //same value and datatype
+    console.log(x);
+}
+
+if(y !== 1) { //same value and datatype
+    console.log(y);
+}
