9e95e5927664d3fc6bcc1337873472a5bdcf46bf
Tahmid Muntaser <115565668+TahmidMuntaser@users.noreply.github.com>
2024-02-10 23:06:24 +0600

    this keyword

--- main4.js
+++ main4.js
@@ -25,3 +25,51 @@
     console.log(student1.dob);
     console.log(student1.getBirthYear());
     console.log(student1.getFullName());
+
+    // this keyword:
+
+    // method -> obj
+    // function -> global or window
+
+    const video = {
+        title: 'a', //property name
+        tags: ['a', 'b', 'c'], 
+        // play: function(){
+        //     console.log(this); // rep video object
+        // }
+
+        showTags(){ // foreach bcoz we use array
+            this.tags.forEach(function(tag){
+                console.log(this.title,tag); // rep window object
+            }, this) // for each has two parameters
+        }
+    }
+
+    video.showTags();
+
+    // play is a method
+
+    video.stop = function(){
+        console.log(this); // rep video object
+    }
+    // video.play();
+    video.stop();
+
+    // regualr function -> global or window
+    function videoPlay(title){
+        console.log(this); 
+    }
+
+    videoPlay(); // rep window object
+
+    // window.alert('hello');
+
+
+    function Video(title){
+        this.title = title;
+        console.log(this); // this rep local obj
+    }
+    const v = new Video('uuu'); // new creates an empty obj
+
+
+
